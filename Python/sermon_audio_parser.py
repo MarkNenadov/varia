728e6e3d1e141a009fce14b521032e4de0a3817d
Mark <marknenadov@gmail.com>
2014-10-21 17:07:53 -0400

    a newer version of this
    cleaned up some old stinky code

--- Python/sermon_audio_parser.py
+++ Python/sermon_audio_parser.py
@@ -7,10 +7,7 @@ A simple python class for using SermonAudio.com's RSS feed.
  and also gives some functionality specific to SermonAudio.com
  that might possibly be useful to someone out there.
 
- Note: To run this, you must have the Universal Feed Parser setup
- under your active Python install
-
- Tested with pylint (10/10 score with default config)
+ Note: To run this, you must have the Universal Feed Parser setup.
 
 AUTHOR
 
@@ -42,58 +39,49 @@ class SermonAudioParser:
     RSS Feed
     """
 
-    SA_URL = "http://www.sermonaudio.com/rss_source.rss"
-    sa_feed = 0
-    entries = 0
+    SERMON_AUDIO_URL = "http://www.sermonaudio.com/rss_source.rss"
+    sermon_audio_feed = None
+    feed_items = None
 
-    def __load_entries(self):
+    def __load_feed_items(self):
         """ parse feed and load items
-	"""
+	    """
 
-        self.sa_feed = feedparser.parse(self.SA_URL )
-        self.entries = self.sa_feed['items']
+        self.sermon_audio_feed = feedparser.parse(self.SERMON_AUDIO_URL )
+        self.feed_items = self.sermon_audio_feed['items']
 
 
     def __init__(self):
         """ Constructor
-	"""
-        self.__load_entries()
+	    """
+        self.__load_feed_items()
 
-    def find_by_title(self, title_match=''):
+    def find_by_title(self, match_value=''):
         """ Do a string search on title
         """ 
         results = []
-        for entry in self.entries:
-            if entry.has_key('title'):
-                if (entry['title'].find(title_match) != -1):
-                    results.append(entry)
+        for item in self.feed_items:
+            if item.has_key('title'):
+                if (item['title'].lower().find(match_value.lower()) != -1):
+                    results.append(item)
         return results
 
-    def find_by_summary(self, summary_match=''):
+    def find_by_summary(self, match_value=''):
         """ Do a string search on summary
-	"""
+	    """
 
         results = []
-        for entry in self.entries:
-            if entry.has_key('summary'):
-                if (entry['summary'].find(summary_match) != -1):
-                    results.append(entry)
+        for item in self.feed_items:
+            if item.has_key('summary'):
+                if (item['summary'].lower().find(match_value.lower()) != -1):
+                    results.append(item)
         return results
 
-    def find_by_all(self, match=''):
+    def find_by_all(self, match_value=''):
         """ Do a string search on summary and title
-	"""
+	    """
 
-        results = []
-        for entry in self.entries:
-            txt_concat = ''
-            if entry.has_key('summary'):
-                txt_concat += entry['summary']
-            if entry.has_key('title'):
-                txt_concat += entry['title']
-            if (txt_concat.find(match) != -1):
-                results.append(entry)
-        return results
+        return list(set(self.find_by_title(match_value) + self.find_by_summary(match_value)))
 
 if __name__ == "__main__":
 
@@ -103,20 +91,20 @@ if __name__ == "__main__":
 
     print "SermonAudioParser Demonstration\n\n"
 
-    print "Feed Items with 'faith' anywhere in their title:"
-    for x in SA_PARSER.find_by_title('faith'):
+    print "Feed Items with 'reformed' anywhere in their title:"
+    for x in SA_PARSER.find_by_title('reformed'):
         print "* " + x['title'] + " - " + x['link']
 
     print " --- "
 
-    print "Feed Items with 'faith' anywhere in their summary:"
-    for x in SA_PARSER.find_by_summary('faith'):
+    print "Feed Items with 'reformed' anywhere in their summary:"
+    for x in SA_PARSER.find_by_summary('reformed'):
         print "* " + x['title'] + " - " + x['link']
 
     print " --- "
         
-    print "Feed Items with 'faith' anywhere in their title or summary:"
-    for x in SA_PARSER.find_by_all('faith'):
+    print "Feed Items with 'reformed' anywhere in their title or summary:"
+    for x in SA_PARSER.find_by_all('reformed'):
         print "* " + x['title'] + " - " + x['link']
 
 
